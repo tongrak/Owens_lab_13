8cf2822174763cbe3ed3e8bbfb79da6dac3f99ab
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-01-22 16:41:04 +0700

    Lab13_1 done! this took 2 restroom break to slove.

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	double holder, counter=1;
+		for(int i=0;i<N-1;i++){
+			for(int j=i;j>=0;j--){
+				if(d[j]<d[j+1]){
+					holder=d[j];
+					d[j]=d[j+1];
+					d[j+1]=holder;
+				}
+			}
+			cout<<"Pass "<<counter<<":";
+			for(int k=0;k<N;k++){cout<<d[k]<<" ";} 
+			cout<<endl;
+			counter++;
+		}
 }
 
 int main(){
